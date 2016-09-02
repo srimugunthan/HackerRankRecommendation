1c58fe3ed8282e11bf8f167761c5b259ab5cd616
Srimugunthan Dhandapani <sdhandap@akamai.com>
2016-09-02 12:23:42 +0530

    add files

---
+++ main.R
@@ -0,0 +1,5 @@
+setwd("/home/sdhandap/HackerRankRecommendation")
+
+
+challengeData <-  read.csv("challenges.csv",   header=TRUE, sep=",", as.is=TRUE)
+submissionData <-  read.csv("submissions.csv",   header=TRUE, sep=",", as.is=TRUE)
